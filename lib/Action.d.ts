d6ecb0a5837013d403409b0723676fbaf38570ec
Sergey Zenchenko <sergeizenchenko@gmail.com>
2017-05-29 16:51:49 +0300

    Refactoring.

--- lib/Action.d.ts
+++ lib/Action.d.ts
@@ -3,14 +3,16 @@ export declare enum ActionState {
     FINISHED = 1,
     FAILED = 2,
 }
-export interface ActionHolder<A extends BaseAction<R>, R> {
-    action: A;
-    state: ActionState;
-    error: Error | null;
-    result: R | null;
+export declare class ActionHolder<A extends BaseAction<R>, R> {
+    readonly action: A;
+    readonly state: ActionState;
+    readonly error: Error | null;
+    readonly result: R | null;
+    constructor(action: A, state: ActionState, error: Error | any, result: any | R);
 }
 export declare const startAction: <A extends BaseAction<R>, R>(action: A) => ActionHolder<A, R>;
 export declare const finishAction: <A extends BaseAction<R>, R>(action: A, result: R) => ActionHolder<A, R>;
 export declare const failAction: <A extends BaseAction<R>, R>(action: A, error: Error) => ActionHolder<A, R>;
 export declare class BaseAction<T> {
+    private stub;
 }
--- src/Action.ts
+++ src/Action.ts
@@ -5,40 +5,33 @@ export enum ActionState {
   FAILED
 }
 
-export interface ActionHolder<A extends BaseAction<R>, R> {
-  action: A;
-  state: ActionState;
-  error: Error | null;
-  result: R | null;
+export class ActionHolder<A extends BaseAction<R>, R> {
+  readonly action: A;
+  readonly state: ActionState;
+  readonly error: Error | null;
+  readonly result: R | null;
+
+  constructor(action: A, state: ActionState, error: Error | any, result: any | R) {
+    this.action = action;
+    this.state = state;
+    this.error = error;
+    this.result = result;
+  }
 }
 
 export const startAction = <A extends BaseAction<R>, R>(action: A): ActionHolder<A, R> => {
-  return {
-    action: action,
-    state: ActionState.RUNNING,
-    error: null,
-    result: null,
-  };
+  return new ActionHolder<A, R>(action, ActionState.RUNNING, null, null);
 };
 
 export const finishAction = <A extends BaseAction<R>, R>(action: A, result: R): ActionHolder<A, R> => {
-  return {
-    action: action,
-    state: ActionState.FINISHED,
-    error: null,
-    result: result,
-  };
+  return new ActionHolder<A, R>(action, ActionState.FINISHED, null, result);
 };
 
 export const failAction = <A extends BaseAction<R>, R>(action: A, error: Error): ActionHolder<A, R> => {
-  return {
-    action: action,
-    state: ActionState.FAILED,
-    error: error,
-    result: null,
-  };
+  return new ActionHolder<A, R>(action, ActionState.FAILED, error, null);
 };
 
 export class BaseAction<T> {
-
+  private stub: T | undefined;
 }
+
